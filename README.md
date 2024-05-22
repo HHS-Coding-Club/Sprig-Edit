b53c891b9bbd4dcfeb6c74c5f260eb7c021bb86d
Jack Spencer <UniformKitten281@gmail.com>
2024-05-21 21:27:22 -0700

    modified:   README.md 	modified:   sprig-edit.js

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
 # Sprig-Edit
 
-Sprig-Edit is a simple text editor for the Sprig.
+This is Sprig Edit, a simple text editor for the Sprig, a handheld console created by Hack Club. It is written in JavaScript, and uses common design patterns to create a simple, yet powerful text editor.
+
+## Features
+
+- **Simple Interface**: Sprig Edit has a simple interface, with a single text area for editing text.
+- **Save and Load**: Sprig Edit can save and load files.
+- **Customizable**: Sprig Edit's background color can be customized with 3 different shades.
+- **Controls**: Since the Sprig has only 8 buttons, Sprig Edit uses a combination of buttons to provide a full set of controls.
+
+### Controls
+
+AD - Move Cursor
+JL - Change Character
+I  - Place Character
+K  - Remove Character
+S  - Space
+W  - Change Color
+
+### Development
+
+The current completed version of Sprig Edit will always be the `./sprig-edit.js` file. The `./src` directory contains the source code for the editor, which is split into multiple files for better organization.
+
+To build the editor, run `npm run build`. This will compile the source code into a single file, which can be used on the Sprig.
+
+### Feature Requests
+
+Since Sprig-Edit is still in development, and is rapidly changing, feature requests are welcome. If you have a feature request, please open an issue on this repository.
+
+### License
+
+Sprig Edit is licensed under the Apache 2.0 License. You can view the license in the `./LICENSE` file.
+
+### Credits
+
+Sprig Edit was created by @Colack, with help from many others. The Sprig is a project by Hack Club, and the Sprig Edit project is not affiliated with Hack Club.
+
+Introducing Sprig-Edit, a simple text editor for Sprig. Sprig-Edit supports 56 different characters, and also supports color. It uses a 16x15 grid to create words and sentences.
+
+Sprig-Edit is still in development, and will be updated for the seeable future.
+
+## Upcoming Features
+
+Sprig-Edit has several features that are either currently in development or are planned to be included in the final release. 
+
+- [X] File Saving
+- [X] File Loading
+- [X] Matrix X/Y reset
+- [X] Customizable background colors
+- [x] addText replaced with createText
+- [X] Editor rendering put into its own function
+- [ ] New version of quill pixel art
+- [X] Numbers in editor
+- [X] Editor displays X0 Y0 instead of just 0 0
+
+Features will be added to this list and checked when they are finished.
--- sprig-edit.js
+++ sprig-edit.js
@@ -59,6 +59,8 @@ const BACKGROUND_BLACK = "B";
 const BACKGROUND_DGREY = "D";
 const BACKGROUND_LGREY = "G";
 
+const COLORS = [color`3`, color`9`, color`6`, color`4`, color`7`, color`2`];
+
 const LOGO_1 = "1";
 const LOGO_2 = "2";
 const LOGO_3 = "3";
@@ -79,10 +81,10 @@ var currentScreen = 0;
 const SPRIG_EDIT = {
   VERSION: {
     MAJOR: 0,
-    MINOR: 2,
-    PATCH: 4,
+    MINOR: 3,
+    PATCH: 0,
   },
-  VERSION_NAME: "Phantom"
+  VERSION_NAME: "Whims"
 };
 
 setLegend(
@@ -285,20 +287,517 @@ const levels = [
 ................
 ................
 ................
-................`,                    // Text Editor
-]
+................`
+];
 
-function setCharacter() {
+const changeCharacter = (currentCharacter) => {
+  const charCode = currentCharacter;
+  if (charCode >= CHAR_START && charCode <= CHAR_END) {
+    if (charCode <= 26) {
+      currentHeldCharacter = String.fromCharCode(charCode + 96);
+    } else if (charCode <= 52) {
+      currentHeldCharacter = String.fromCharCode(charCode + 64 - 26);
+    } else {
+      const specialChars = ["!", ".", "?", ",", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+      currentHeldCharacter = specialChars[charCode - 53];
+    }
+  }
+  console.log(currentCharacter, currentHeldCharacter);
+};
 
+const changeTextColor = () => {
+  textColor = COLORS[currentColor];
+};
+
+const createScreens = (screen) => {
+  clearText();
+  switch (screen) {
+    case 0:
+      createMenuTitle(5, "Sprig Edit");
+      createText(`v${SPRIG_EDIT.VERSION.MAJOR}.${SPRIG_EDIT.VERSION.MINOR}.${SPRIG_EDIT.VERSION.PATCH}`, 7, 4, color`2`);
+      createText("Press S To", 5, 7, color`2`);
+      createText("Start", 8, 9, color`2`);
+      createText("Colack 2024", 2, 15, color`2`);
+      break;
+    case 1:
+      createMenuTitle(4, "Menu Options");
+      ["W - ChangeLog", "A - Credits", "S - Examples", "D - About", " ", "I - New File", "J - Controls", "K - Customize", "L - Load File"]
+        .forEach((text, idx) => createText(text, 2, 3 + idx, color`2`));
+      break;
+    case 2:
+      loadMatrix();
+      createText("Color", 2, 15, textColor);
+      createText(currentHeldCharacter, 8, 15, color`2`);
+      createText(`X${MATRIX_X}`, 10, 15, color`2`);
+      createText(`Y${MATRIX_Y}`, 14, 15, color`2`);
+      break;
+    case 3:
+      createMenuTitle(6, "Credits");
+      ["By Jack S.", "", "Testers:", "- Joe", "- Nat", "- Trent", "- Jared", "- Phin", "", "", "Thanks for using", "Sprig Edit"]
+        .forEach((text, idx) => createText(text, 2, 3 + idx, color`2`));
+      break;
+    case 4:
+      createMenuTitle(5, "Change Log");
+      [`Sprig-Edit ${SPRIG_EDIT.VERSION.MAJOR}.${SPRIG_EDIT.VERSION.MINOR}.${SPRIG_EDIT.VERSION.PATCH}`, `${SPRIG_EDIT.VERSION_NAME} Update`, "Added:", "- TBD"]
+        .forEach((text, idx) => createText(text, 2, 2 + idx, color`2`));
+      break
+    case 5:
+      createMenuTitle(6, "Controls");
+      createText("AD - Move Cursor", 2, 2, color`2`);
+      createText("JL - Change Char", 2, 3, color`2`);
+      createText("I  - Place Char ", 2, 4, color`2`);
+      createText("K  - Remove Char", 2, 5, color`2`);
+      createText("S  - Space      ", 2, 6, color`2`);
+      createText("W  -", 2, 7, color`2`);
+      createText("C", 7, 7, color`3`);
+      createText("o", 8, 7, color`9`);
+      createText("l", 9, 7, color`6`);
+      createText("o", 10, 7, color`D`);
+      createText("r", 11, 7, color`5`);
+      createText("s", 12, 7, color`H`);
+      createText("To save, go to", 2, 9, color`2`);
+      createText("the last line", 2, 10, color`2`);
+      createText("and press S!", 2, 11, color`2`);
+      break;
+    case 6:
+      createMenuTitle(6, "Examples");
+      ["W - Example Text", "A - Hello World", "S - Lorem ipsum", "D - Welcome", "I - Thanks", "J - Program", "K - Roses", "L - Princess"]
+        .forEach((text, idx) => createText(text, 2, 3 + idx, color`2`));
+      break;
+    case 7:
+      createMenuTitle(8, "About");
+      ["Sprig Edit, a si", "mple text editor", "for Sprig.", "It supports 66", "different charac", "ters, and color.", "You can find the", "code at github.c", "om/colack/sprig-", "edit!"]
+        .forEach((text, idx) => createText(text, 2, 3 + idx, color`2`));
+      break;
+    case 8:
+      createMenuTitle(6, "New File");
+
+      createText("AD - Move Cursor", 2, 2, color`2`);
+      createText("JL - Change Char", 2, 3, color`2`);
+      createText("I  - Place Char ", 2, 4, color`2`);
+      createText("K  - Remove Char", 2, 5, color`2`);
+      createText("S  - Space      ", 2, 6, color`2`);
+      createText("W  -", 2, 7, color`2`);
+      createText("C", 7, 7, color`3`);
+      createText("o", 8, 7, color`9`);
+      createText("l", 9, 7, color`6`);
+      createText("o", 10, 7, color`D`);
+      createText("r", 11, 7, color`5`);
+      createText("s", 12, 7, color`H`);
+      createText("To save, go to", 2, 9, color`2`);
+      createText("the last line", 2, 10, color`2`);
+      createText("and press S!", 2, 11, color`2`);
+      createText("Press any key", 2, 13, color`2`);
+      createText("to Start.", 2, 14, color`2`);
+      break;
+    case 9:
+      createMenuTitle(8, "Save");
+      ["W - Slot 1", "A - Slot 2", "S - Slot 3", "D - Slot 4", "I - Slot 5", "J - Slot 6", "K - Slot 7", "L - Slot 8"]
+        .forEach((text, idx) => createText(text, 2, 2 + idx, color`2`));
+      break;
+    case 10:
+      createMenuTitle(8, "Load");
+      ["W - Slot 1", "A - Slot 2", "S - Slot 3", "D - Slot 4", "I - Slot 5", "J - Slot 6", "K - Slot 7", "L - Slot 8"]
+        .forEach((text, idx) => createText(text, 2, 2 + idx, color`2`));
+      break;
+    case 11:
+      createMenuTitle(6, "Customize");
+      ["BkGr Color:", "W - Black", "A - Dark Grey", "S - Grey"]
+        .forEach((text, idx) => createText(text, 2, 2 + idx, color`2`));
+      break;
+  }
+};
+
+const createMenuTitle = (x, text) => {
+  createText(text, x, 0, color`2`);
+  createText("(==============)", 2, 1, color`2`);
+};
+
+const loadMatrix = () => {
+  for (let y = 0; y < 15; y++) {
+    for (let x = 0; x < 16; x++) {
+      if (x === MATRIX_X && y === MATRIX_Y) {
+        let newX = x + 2;
+        createText("/", newX, y, textColor);
+      } else {
+        let newX = x + 2;
+        createText(matrix[y][x], newX, y, colorMatrix[y][x]);
+      }
+    }
+  }
 }
 
-setMap(levels[level])
+function handleInput(input) {
+  switch (input) {
+    case "w":
+      switch (currentScreen) {
+        case 1:
+          currentScreen = 4;
+          break;
+        case 2:
+          if (currentColor == 5) {
+            currentColor = 0;
+          } else {
+            currentColor++;
+          }
+          changeTextColor();
+          break;
+        case 6:
+          
+          resetColorMatrix();
+          currentScreen = 2;
+          break;
+        case 8:
+          matrix = stringToMatrix(SAMPLES[0]);
+          resetColorMatrix();
+          currentScreen = 2;
+          break;
+        case 9:
+          saveFile(0);
+          currentScreen = 1;
+          break;
+        case 10:
+          loadFile(0);
+          currentScreen = 2;
+          break;
+        case 11:
+          setBackground(BACKGROUND_BLACK);
+          currentScreen = 1;
+          break;
+      }
+      break;
+    case "a":
+      switch (currentScreen) {
+        case 1:
+          currentScreen = 3;
+        case 2:
+          if (MATRIX_X == 0) {
+            if (MATRIX_Y > 0) {
+              MATRIX_Y--;
+              MATRIX_X = 15;
+            }
+          } else {
+            MATRIX_X--;
+          }
+          break;
+        case 6:
+          matrix = stringToMatrix(SAMPLES[2]);
+          resetColorMatrix();
+          currentScreen = 2;
+          break;
+        case 8:
+          matrix = stringToMatrix(SAMPLES[0]);
+          resetColorMatrix();
+          currentScreen = 2;
+          break;
+        case 9:
+          saveFile(1);
+          currentScreen = 1;
+          break;
+        case 10:
+          loadFile(1);
+          currentScreen = 2;
+          break;
+        case 11:
+          setBackground(BACKGROUND_DGREY);
+          currentScreen = 1;
+          break;
+      }
+      break;
+    case "s":
+      switch (currentScreen) {
+        case 3:
+        case 4:
+        case 5:
+        case 7:
+        case 0:
+          currentScreen = 1;
+          setMap(levels[1]);
+          break;
+        case 1:
+          currentScreen = 6;
+          break;
+        case 2:
+          matrix[MATRIX_Y][MATRIX_X + 1] = " ";
+          if (MATRIX_X < 15) {
+            MATRIX_X += 2;
+          } else if (MATRIX_Y < 14) {
+            MATRIX_X = 0;
+            MATRIX_Y++;
+          } else if (MATRIX_X == 15 && MATRIX_Y == 14) {
+            currentScreen = 9;
+          } else if (MATRIX_X == 16 && MATRIX_Y == 14) {
+            console.log("EASTER EGG ACTIVATED");
+          }
+          break;
+        case 6:
+          matrix = stringToMatrix(SAMPLES[3]);
+          resetColorMatrix();
+          currentScreen = 2;
+          break;
+        case 8:
+          matrix = stringToMatrix(SAMPLES[0]);
+          resetColorMatrix();
+          currentScreen = 2;
+          break;
+        case 9:
+          saveFile(2);
+          currentScreen = 1;
+          break;
+        case 10:
+          loadFile(2);
+          currentScreen = 2;
+          break;
+        case 11:
+          setBackground(BACKGROUND_LGREY);
+          currentScreen = 1;
+          break;
+      }
+      break;
+    case "d":
+      switch (currentScreen) {
+        case 1:
+          currentScreen = 7;
+          break;
+        case 2:
+          if (MATRIX_X == 15 && MATRIX_Y < 14) {
+            MATRIX_Y++;
+            MATRIX_X = 0;
+          } else if (MATRIX_X < 15) {
+            MATRIX_X++;
+          }
+          break;
+        case 6:
+          matrix = stringToMatrix(SAMPLES[4]);
+          resetColorMatrix();
+          currentScreen = 2;
+          break;
+        case 8:
+          matrix = stringToMatrix(SAMPLES[0]);
+          resetColorMatrix();
+          currentScreen = 2;
+          break;
+        case 9:
+          saveFile(3);
+          currentScreen = 1;
+          break;
+        case 10:
+          loadFile(3);
+          currentScreen = 2;
+          break;
+      }
+      break;
+    case "i":
+      switch (currentScreen) {
+        case 1:
+          currentScreen = 8;
+          break;
+        case 2:
+          matrix[MATRIX_Y][MATRIX_X] = currentHeldCharacter;
+          colorMatrix[MATRIX_Y][MATRIX_X] = textColor;
+          break;
+        case 6:
+          matrix = stringToMatrix(SAMPLES[5]);
+          resetColorMatrix();
+          currentScreen = 2;
+          break;
+        case 7:
+          currentScreen = 8;
+          break;
+        case 8:
+          matrix = stringToMatrix(SAMPLES[0]);
+          resetColorMatrix();
+          currentScreen = 2;
+          break;
+        case 9:
+          saveFile(4);
+          currentScreen = 1;
+          break;
+        case 10:
+          loadFIle(4);
+          currentScreen = 2;
+          break;
+      }
+      break;
+    case "j":
+      switch (currentScreen) {
+        case 1:
+          currentScreen = 5;
+          break;
+        case 2:
+          if (currentCharacter == 1) {
+            currentCharacter = 66;
+          } else {
+            currentCharacter--;
+          }
+          changeCharacter();
+          break;
+        case 6:
+          matrix = stringToMatrix(SAMPLES[6]);
+          resetColorMatrix();
+          currentScreen = 2;
+          break;
+        case 8:
+          matrix = stringToMatrix(SAMPLES[0]);
+          resetColorMatrix();
+          currentScreen = 2;
+          break;
+        case 9:
+          saveFile(5);
+          currentScreen = 1;
+          break;
+        case 10:
+          loadFile(5);
+          currentScreen = 2;
+          break;
+      }
+      break;
+    case "k":
+      switch (currentScreen) {
+        case 1:
+          currentScreen = 11;
+        case 2:
+          matrix[MATRIX_Y][MATRIX_X] = " ";
+          break;
+        case 6:
+          matrix = stringToMatrix(SAMPLES[7]);
+          resetColorMatrix();
+          currentScreen = 2;
+          break;
+        case 8:
+          matrix = stringToMatrix(SAMPLES[0]);
+          resetColorMatrix();
+          currentScreen = 2;
+          break;
+        case 9:
+          saveFile(6);
+          currentScreen = 1;
+          break;
+        case 10:
+          loadFile(6);
+          currentScreen = 2;
+          break;
+      }
+      break;
+    case "l":
+      switch (currentScreen) {
+        case 1:
+          currentScreen = 10;
+          break;
+        case 2:
+          if (currentCharacter == 66) {
+            currentCharacter = 1;
+          } else {
+            currentCharacter++;
+          }
+          changeCharacter();
+          break;
+        case 6:
+          matrix = stringToMatrix(SAMPLES[8]);
+          resetColorMatrix();
+          currentScreen = 2;
+          break;
+        case 8:
+          matrix = stringToMatrix(SAMPLES[0]);
+          resetColorMatrix();
+          currentScreen = 2;
+          break;
+        case 9:
+          saveFile(7);
+          currentScreen = 1;
+          break;
+        case 10:
+          loadFile(7);
+          currentScreen = 2;
+          break;
+      }
+      break;
+  }
+}
+const stringToMatrix = (text) => {
+  const matrix = Array.from({ length: 15 }, () => Array(16).fill(' '));
+  let row, col = 0;
 
-setPushables({
-  [ player ]: []
-})
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === '\n') {
+      row++;
+      col = 0;
+    } else {
+      if (row < 15 && col < 16) {
+        matrix[row][col] = text[i];
+        col++;
+      }
+    }
+  }
 
+  return matrix;
+};
 
-afterInput(() => {
-  
-})
+const drawCursor = () => {
+  colorMatrix[MATRIX_Y][MATRIX_X] = textColor;
+};
+
+const clearTiles = () => {
+  for (let y = 0; y < 15; y++) {
+    for (let x = 0; x < 16; x++) {
+      createText(matrix[y][x], x, y, colorMatrix[y][x]);
+    }
+  }
+}
+
+const resetMatrixPositions = () => {
+  MATRIX_X, MATRIX_Y = 0;
+}
+
+const resetColorMatrix = () => {
+  colorMatrix = Array.from({ length: 15 }, () => Array(16).fill(color`2`));
+}
+
+const matrixToString = (matrix) => {
+  let result = "";
+  for (let row of matrix) {
+    result += row.join("") + "\n";
+  }
+  return result.trim();
+}
+
+const matrixToColor = (colorMatrix) => {
+  const array = [];
+  for (let row of colorMatrix) {
+    for (let col of row) {
+      array.push(col);
+    }
+  }
+  return array;
+}
+
+const saveFile = (num) => {
+  MATRIX_SAVES[num] = matrixToString(matrix);
+  MATRIX_COLOR[num] = matrixToColor(colorMatrix);
+}
+
+const loadFile = (num) => {
+  matrix = stringToMatrix(MATRIX_SAVES[num]);
+  colorMatrix = stringToMatrix(MATRIX_COLOR[num]);
+}
+
+const createText = (text, x, y, color) => {
+  addText(text, { x: x, y: y, color: color });
+};
+
+onInput("w", () => { handleInput("w"); });
+onInput("a", () => { handleInput("a"); });
+onInput("s", () => { handleInput("s"); });
+onInput("d", () => { handleInput("d"); });
+onInput("i", () => { handleInput("i"); });
+onInput("j", () => { handleInput("j"); });
+onInput("k", () => { handleInput("k"); });
+onInput("l", () => { handleInput("l"); });
+
+setInterval(function() {
+  createScreens(currentScreen);
+}, 30);
+
+setBackground(BACKGROUND_BLACK);
+createText("Loading...", 5, 8, color`2`);
+setMap(levels[level]);
